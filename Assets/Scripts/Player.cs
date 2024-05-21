3615c820028d0cf7f1681b41ae2c92d6a23aa10d
Unity-Gruppe2 <253791+unity2@usn.no>
2024-05-21 21:05:08 +0200

    Add player movement
    Oppgave 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,5 +8,21 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5f; // Movement speed
+    private Rigidbody2D rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * speed;
+
+        rb.velocity = movement;
+    }
 }
