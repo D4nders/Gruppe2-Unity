93c28f5a0855f5ff4c6023a454c7a5e85303b2b0
Unity-Gruppe2 <253791+unity2@usn.no>
2024-05-23 19:04:58 +0200

    Add Weapon kunai prefab

---
+++ Assets/Scripts/KunaiBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiBehaviour : ProjectileWeaponBehaviour
+{
+    WeaponKunai wk;
+
+    protected override void Start()
+    {
+        base.Start();
+        wk = FindObjectOfType<WeaponKunai>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
